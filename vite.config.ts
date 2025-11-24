bdee7540bbaddeefcd777e0a77e12bf6f62aaef9
Zuzanna Banach <yrt@imac.speedport.ip>
2025-11-24 14:37:44 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/various-degrees-landing/", // <-- add this line
   plugins: [
     react(),
     runtimeErrorOverlay(),
