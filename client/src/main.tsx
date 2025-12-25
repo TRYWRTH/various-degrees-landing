8d20f39557f7cf736241f0268bff90bbf57ecbee
TRYWRTH <zuzanna.banach@proton.me>
2025-12-25 19:36:21 +0000

    Import Tailwind index.css in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Router } from "wouter";
+import "./index.css";
 
 const root = document.getElementById("root");
-console.log("root element:", root);
 
 ReactDOM.render(
   <Router>
